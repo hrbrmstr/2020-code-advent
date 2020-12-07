811786ea655de79aa5749cef23eb7db494a63892
hrbrmstr <bob@rud.is>
2020-12-07 07:05:36 -0500

    Day 5 R and Python

---
+++ R/05.py
@@ -0,0 +1,43 @@
+import math
+import numpy as np  
+
+with open("../input/05-01.txt") as f:
+  input = f.read().splitlines()
+
+def partition(seq, lb, ub, trigger, ret):
+  
+  for idx, part in enumerate(seq):
+    amt = math.floor((ub - lb)/2) + 1
+    if (part == trigger):
+      ub -= amt
+    else:
+      lb += amt
+  
+  return(ub if (seq[-1] == trigger) else lb)
+
+
+def get_seat(x):
+  return(
+    (partition(x[0:7],  0, 127, "F", "F") * 8) + 
+     partition(x[7:10], 0,   7, "L", "L") 
+  )
+
+res = [ get_seat(bpass) for bpass in input ]
+
+# 05-01
+
+res.sort()
+
+max(res)
+
+# 05-02
+
+res.insert(0, res[0])
+idx = np.where(np.diff(res) > 1)
+res.pop(0)
+res[idx[0][0]] - 1
+
+
+
+
+
